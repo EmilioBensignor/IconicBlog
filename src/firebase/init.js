d2900f0221d6dcdf7430c3e20c69e0a69af83a2f
Lio <liobensignor@gmail.com>
2024-07-02 16:18:20 -0300

    Init.js

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -16,3 +16,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseApp);
+const functions = getFunctions(firebaseApp);
+export { auth, functions, firebaseApp, analytics };
